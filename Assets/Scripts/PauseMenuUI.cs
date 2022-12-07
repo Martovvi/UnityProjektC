7baf904ecae9dd44baa0b995a371182c0f3ac409
Annsa2102 <93792629+Annsa2102@users.noreply.github.com>
2022-12-07 18:25:09 +0100

    added UI

---
+++ Assets/Scripts/PauseMenuUI.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenuUI : MonoBehaviour
+{
+    public GameObject PauseMenu;
+
+    public void Awake()
+    {
+        PauseMenu.SetActive(false);
+    }
+
+    private void PauseGame()
+    {
+        PauseMenu.SetActive(true);
+
+        Time.timeScale = 0;
+
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        PauseMenu.SetActive(false);
+
+    }
+    public void BackToMainMenu()
+    {
+
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PauseMenu.activeSelf)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+}
+
