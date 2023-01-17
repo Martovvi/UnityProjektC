3086817aa9e67cc9fdf72d0286fdb543b6b1556a
M <55866297+Martovvi@users.noreply.github.com>
2023-01-17 17:37:41 +0100

    Added animated health bar + various bug fixes

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    private float health;
+    private float lerpTimer;
+    public float maxHealth = 100f;
+    public float chipSpeed = 2f;
+    public Image backHealthBar;
+    public Image frontHealthBar;
+    public Gradient gradient;
+    public TextMeshProUGUI digits;
+
+    private void Start()
+    {
+        health = maxHealth;
+        digits = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        health = Mathf.Clamp(health, 0, maxHealth);
+        frontHealthBar.color = gradient.Evaluate((frontHealthBar.fillAmount));
+        UpdateHealthUI();
+    }
+
+    public void UpdateHealthUI()
+    {
+        digits.text = health.ToString(CultureInfo.CurrentCulture);
+        float fillF = frontHealthBar.fillAmount;
+        float fillB = backHealthBar.fillAmount;
+        float healthNormalized = health / maxHealth;
+        if (fillB > healthNormalized)
+        {
+            frontHealthBar.fillAmount = healthNormalized;
+            Color backHealthColor = new Color(0.49f, 0.06f, 0.06f, 1f);
+            backHealthBar.color = backHealthColor;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete *= percentComplete;
+            backHealthBar.fillAmount = Mathf.Lerp(fillB, healthNormalized, percentComplete);
+        }
+        if (fillF < healthNormalized)
+        {
+            frontHealthBar.fillAmount = healthNormalized;
+            Color backHealthColor = new Color(0.06f, 0.49f, 0.06f, 1f);
+            backHealthBar.color = backHealthColor;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete *= percentComplete;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillF, backHealthBar.fillAmount, percentComplete);
+        }
+    }
+
+    public void Damage(float amount)
+    {
+        health -= amount;
+        lerpTimer = 0f;
+    }
+
+    public void Heal(float amount)
+    {
+        health += amount;
+        lerpTimer = 0f;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,6 +20,7 @@ public class PlayerMovement : MonoBehaviour {
     // Stats
     public float maxHealth = 100f;
     public float health = 100f;
+    public bool isDead = false;
     
     // Movement
     [Header("Movement")] 
@@ -62,6 +63,7 @@ public class PlayerMovement : MonoBehaviour {
     private Vector3 moveDirection;
     public MovementState state;
     public GameObject blood;
+    public HealthBar healthBar;
     public enum MovementState
     {
         WALKING,
@@ -338,6 +340,7 @@ public class PlayerMovement : MonoBehaviour {
     // Reduces health on damage and kills player
     public void TakeDamage(float damageAmount)
     {
+        healthBar.Damage(damageAmount);
         health -= damageAmount;
         
         Instantiate(blood,
@@ -346,7 +349,8 @@ public class PlayerMovement : MonoBehaviour {
 
         if (health <= 0)
         {
-            Destroy(gameObject);
+            this.enabled = false;
+            isDead = true;
         }
     }
 
